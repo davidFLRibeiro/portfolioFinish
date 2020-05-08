50563563830098d751b954c708b8dd2410c24539
David Ribeiro <davidflribeiro87@gmail.com>
2020-05-08 20:14:27 +0000

    new changes

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function showHome() {
-  $('.home').on('click', function (event) {
-    console.log('work');
-  });
-}
-$(showHome);
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
 
-function scroll() {
-  $('a').click(function (e) {
-    var jump = $(this).attr('href');
-    var new_position = $(jump).offset();
-    $('html, body').stop().animate({ scrollTop: new_position.top }, 700);
-    e.preventDefault();
-  });
-}
-$(scroll);
+  $('html, body').animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    1000
+  );
+});
